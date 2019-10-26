849cacef3252de8def8983c884c3cce9317a7b8d
Katie Midgley <kjm891@student.bham.ac.uk>
2019-10-26 23:54:31 +0100

    Added winning function

--- gameAI.py
+++ gameAI.py
@@ -35,3 +35,41 @@ def move(board):
         return 9
     else: 
         return freeSpaces[0]
+
+def isWin(board):
+    if ((board[0][0] == 'X') and (board[0][1] == 'X') and (board[0][2] == 'X')):
+        return 'X'
+    elif ((board[1][0] == 'X') and (board[1][1] == 'X') and (board[1][2] == 'X')):
+        return 'X'
+    elif ((board[2][0] == 'X') and (board[2][1] == 'X') and (board[2][2] == 'X')):
+        return 'X'
+    elif ((board[0][0] == 'X') and (board[1][0] == 'X') and (board[2][0] == 'X')):
+        return 'X'
+    elif ((board[0][1] == 'X') and (board[1][1] == 'X') and (board[2][1] == 'X')):
+        return 'X'
+    elif ((board[0][2] == 'X') and (board[1][2] == 'X') and (board[2][2] == 'X')):
+        return 'X'
+    elif ((board[0][0] == 'X') and (board[1][1] == 'X') and (board[2][2] == 'X')):
+        return 'X'
+    elif ((board[0][2] == 'X') and (board[1][1] == 'X') and (board[2][0] == 'X')):
+        return 'X'
+    
+    elif ((board[0][0] == 'O') and (board[0][1] == 'O') and (board[0][2] == 'O')):
+        return 'O'
+    elif ((board[1][0] == 'O') and (board[1][1] == 'O') and (board[1][2] == 'O')):
+        return 'O'
+    elif ((board[2][0] == 'O') and (board[2][1] == 'O') and (board[2][2] == 'O')):
+        return 'O'
+    elif ((board[0][0] == 'O') and (board[1][0] == 'O') and (board[2][0] == 'O')):
+        return 'O'
+    elif ((board[0][1] == 'O') and (board[1][1] == 'O') and (board[2][1] == 'O')):
+        return 'O'
+    elif ((board[0][2] == 'O') and (board[1][2] == 'O') and (board[2][2] == 'O')):
+        return 'O'
+    elif ((board[0][0] == 'O') and (board[1][1] == 'O') and (board[2][2] == 'O')):
+        return 'O'
+    elif ((board[0][2] == 'O') and (board[1][1] == 'O') and (board[2][0] == 'O')):
+        return 'O'
+
+    else:
+        return 'No-one!'
