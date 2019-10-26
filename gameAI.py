91c5823ccef18e552a003cdec9e53b6c073d9755
Katie Midgley <kjm891@student.bham.ac.uk>
2019-10-27 00:22:46 +0100

    added print board function

--- gameAI.py
+++ gameAI.py
@@ -73,3 +73,8 @@ def isWin(board):
 
     else:
         return 'No-one!'
+
+def printBoard(board):
+    return (board[0][0] + "|" + board[0][1] + "|" + board[0][2] + "\n" + "-----" + "\n" + 
+            board[1][0] + "|" + board[1][1] + "|" + board[1][2] + "\n" + "-----" + "\n" +
+            board[2][0] + "|" + board[2][1] + "|" + board[2][2])
